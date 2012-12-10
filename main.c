36ed47a9f1115c70e9da56690ff2edf14da82961
Miroslav Scaldov <scaldov@scyld.sky>
2012-12-10 08:43:28 +0400

    AD8950

--- main.c
+++ main.c
@@ -20,6 +20,156 @@
 #define HD44780_SH    4
 #define HD44780_PORT  GPIOC
 
+#define AD9850_DATA  GPIOB
+#define AD9850_SYNC  GPIOC
+#define AD9850_SDATA 0x40
+#define AD9850_WCLK  0x80
+#define AD9850_FQUP  0x40
+#define AD9850_RST   0x20
+
+void ad8950_init()
+{
+  uint8_t p;
+//  CRITICAL_STORE;
+//  CRITICAL_START();
+  GPIO_Init(AD9850_DATA, 0xFF, GPIO_MODE_OUT_PP_HIGH_FAST);
+  GPIO_Init(AD9850_SYNC, AD9850_WCLK | AD9850_FQUP | AD9850_RST, GPIO_MODE_OUT_PP_HIGH_FAST);
+  p = (uint8_t)AD9850_SYNC->ODR & ~(AD9850_FQUP | AD9850_WCLK | AD9850_RST);
+  AD9850_DATA->ODR = 0;
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p | AD9850_RST;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p | AD9850_FQUP;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+//  CRITICAL_END();
+}
+
+void ad8950_freq(uint32_t freq)
+{
+  uint8_t p;
+  CRITICAL_STORE;
+  CRITICAL_START();
+  p = (uint8_t)AD9850_SYNC->ODR & ~(AD9850_FQUP | AD9850_WCLK | AD9850_RST);
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_DATA->ODR = 0;
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_DATA->ODR = (freq >> 24) & 0xFF;
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_DATA->ODR = (freq >> 16) & 0xFF;
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_DATA->ODR = (freq >> 8) & 0xFF;
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  AD9850_DATA->ODR = (freq >> 0) & 0xFF;
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  //*/
+  AD9850_SYNC->ODR = p | AD9850_FQUP;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  CRITICAL_END();
+}
+
+void ad8950_freqs(uint32_t freq)
+{
+  uint8_t p;
+  uint8_t i, d;
+  CRITICAL_STORE;
+  CRITICAL_START();
+  p = (uint8_t)AD9850_SYNC->ODR & ~(AD9850_FQUP | AD9850_WCLK | AD9850_RST);
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  d = (freq ) & 0xFF;
+  for(i = 0; i < 8; i++)
+  {
+    AD9850_DATA->ODR = ((d>>1) & 1) ? 0xC0 : 0;
+    AD9850_SYNC->ODR = p | AD9850_WCLK;
+    asm("nop\n");
+    AD9850_SYNC->ODR = p;
+    asm("nop\n");
+  }
+  d = (freq >> 8) & 0xFF;
+  for(i = 0; i < 8; i++)
+  {
+    AD9850_DATA->ODR = ((d>>1) & 1) ? 0xC0 : 0;
+    AD9850_SYNC->ODR = p | AD9850_WCLK;
+    asm("nop\n");
+    AD9850_SYNC->ODR = p;
+    asm("nop\n");
+  }
+  d = (freq >> 16) & 0xFF;
+  for(i = 0; i < 8; i++)
+  {
+    AD9850_DATA->ODR = ((d>>1) & 1) ? 0xC0 : 0;
+    AD9850_SYNC->ODR = p | AD9850_WCLK;
+    asm("nop\n");
+    AD9850_SYNC->ODR = p;
+    asm("nop\n");
+  }
+  d = (freq >> 24) & 0xFF;
+  for(i = 0; i < 8; i++)
+  {
+    AD9850_DATA->ODR = ((d>>1) & 1) ? 0xC0 : 0;
+    AD9850_SYNC->ODR = p | AD9850_WCLK;
+    asm("nop\n");
+    AD9850_SYNC->ODR = p;
+    asm("nop\n");
+  }
+  d = 0;
+  for(i = 0; i < 8; i++)
+  {
+    AD9850_DATA->ODR = ((d>>1) & 1) ? 0xC0 : 0;
+    AD9850_SYNC->ODR = p | AD9850_WCLK;
+    asm("nop\n");
+    AD9850_SYNC->ODR = p;
+    asm("nop\n");
+  }
+  AD9850_SYNC->ODR = p | AD9850_FQUP;
+  asm("nop\n");
+  AD9850_SYNC->ODR = p;
+  asm("nop\n");
+  CRITICAL_END();
+}
+
+
+void ad8950_phase(uint8_t phase)
+{
+  uint8_t p;
+  CRITICAL_STORE;
+  CRITICAL_START();
+  p = (uint8_t)AD9850_SYNC->ODR & ~(AD9850_FQUP | AD9850_WCLK | AD9850_RST);
+  AD9850_DATA->ODR = phase & 0xF8;
+  AD9850_SYNC->ODR = p | AD9850_WCLK;
+  AD9850_SYNC->ODR = p;
+  AD9850_SYNC->ODR = p | AD9850_FQUP;
+  AD9850_SYNC->ODR = p;
+  CRITICAL_END();
+}
+
 void hd44780_cmd(uint8_t cmd)
 {
   uint8_t p = (uint8_t)HD44780_PORT->ODR & ~(HD44780_DATA | HD44780_RS | HD44780_E | HD44780_RW);
@@ -97,7 +247,7 @@ static NO_REG_SAVE void btn_thread_func (void)
 static NO_REG_SAVE void io_thread_func (void)
 {
   int i, j, k;
-  //
+  /*
   krn_sleep(KRN_FREQ/2);
   hd44780_cmd(0x28);
   krn_sleep(1);
@@ -117,22 +267,26 @@ static NO_REG_SAVE void io_thread_func (void)
   krn_sleep(1);
   hd44780_out('.');
   krn_sleep(1);
-  //
+  */
+  ad8950_init();
+  //ad8950_freq(463856468); //13.5MHz
+  //ad8950_freqs(927712936); //27MHz
+  ad8950_freqs(8590); //250Hz
   while(1)
   {
     adc_data[i] = i&0xFF;
     i = (i+1) & 0xFF;
     krn_mutex_lock(&mutex_printf);
-    j = GPIO_ReadInputPin(GPIOB, GPIO_PIN_1) ? 1 : 0;
-    k = GPIO_ReadInputPin(GPIOB, GPIO_PIN_0) ? 1 : 0;
+    //j = GPIO_ReadInputPin(GPIOB, GPIO_PIN_1) ? 1 : 0;
+    //k = GPIO_ReadInputPin(GPIOB, GPIO_PIN_0) ? 1 : 0;
     printf("%d\t%d\tsec=%d\tT=%d\tP=%d\n", i, g_cnt, krn_timer_cnt / KRN_FREQ, j, k);
     krn_mutex_unlock(&mutex_printf);
     sprintf(g_str, "%d", krn_timer_cnt / KRN_FREQ);
     k = strlen(g_str);
     for(j = 0; j < k; j++)
     {
-        hd44780_out(g_str[j]);
-        krn_sleep(1);
+        //hd44780_out(g_str[j]);
+        //krn_sleep(1);
     }
     krn_sleep(KRN_FREQ/10);
   }
@@ -146,8 +300,8 @@ int main( void )
   flag_led = 0;
   CLK_DeInit();
   CLK_HSICmd(ENABLE);
-  CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV2);  //8-8 still normal
-  CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV2);
+  CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);  //8-8 still normal
+  CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
   GPIO_DeInit(GPIOD);
   GPIO_Init(GPIOD, GPIO_PIN_0, GPIO_MODE_OUT_PP_HIGH_FAST);
   GPIO_Init(GPIOB, GPIO_PIN_7, GPIO_MODE_IN_PU_NO_IT);
